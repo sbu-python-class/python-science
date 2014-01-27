34d2ea86632a4350f07ccbe46614f2dce46caffb
Michael Zingale <zingale@localhost.localdomain>
2014-01-26 19:57:04 -0500

    overflow in python

---
+++ examples/numerical-basics/overflow/overflow.py
@@ -0,0 +1,16 @@
+iold = -1
+i = 1
+
+type_init = type(i)
+print "type currently: ", type_init
+
+while (i > iold):
+    print i
+    iold = i
+    i *= 2
+
+    if (not type(i) == type_init):
+        print "type changed, now: ", type(i)
+        break
+
+print i
