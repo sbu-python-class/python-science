f1d93c87cb38131fece15406ed5c4068e2a7d862
Michael Zingale <zingale@localhost.localdomain>
2014-02-27 16:02:38 -0500

    regular expressions example

---
+++ examples/python-snippets/regex.py
@@ -0,0 +1,19 @@
+import re
+
+strings = [r"<a>this is my string</a>",
+           r"<b>this is a different string</b>"]
+
+# this is the pattern that we will match -- it has 3 groups
+re_test = r"<(\w)>(.*)</(\w)>"
+
+
+for s in strings:
+    a = re.search(re_test, s)
+
+    if not a == None:
+        if a.group(1) == a.group(3):
+            # we found a match
+            print "string in '{}' tags is: {}".format(a.group(1), a.group(2))
+
+
+
