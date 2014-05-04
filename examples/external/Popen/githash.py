2c39cc75fea09955efbbc933401ba23cbee179ec
Michael Zingale <zingale@localhost.localdomain>
2014-05-04 17:10:33 -0400

    good example

--- examples/external/Popen/githash.py
+++ examples/external/Popen/githash.py
@@ -1,10 +1,13 @@
 import subprocess
+import string
 
-prog = ["git", "rev-parse", "--abbrev-ref", "HEAD"]
+# issue the command 'git rev-parse HEAD'
+prog = ["git", "rev-parse", "HEAD"]
 p0 = subprocess.Popen(prog, stdout=subprocess.PIPE,
-                      stderr=subprocess.STDOUT)
+                      stderr=subprocess.PIPE)
 stdout0, stderr0 = p0.communicate()
 
 print "stdout: {}".format(stdout0)
 print "stderr: {}".format(stderr0)
+
 
