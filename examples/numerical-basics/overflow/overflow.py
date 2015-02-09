d03525a96f0e04a5d16ecd428a6ddeeb8c6b6156
Michael Zingale <michael.zingale@stonybrook.edu>
2015-02-09 11:50:36 -0500

    make python3 compatible

--- examples/numerical-basics/overflow/overflow.py
+++ examples/numerical-basics/overflow/overflow.py
@@ -1,16 +1,18 @@
+from __future__ import print_function
+
 iold = -1
 i = 1
 
 type_init = type(i)
-print "type currently: ", type_init
+print("type currently: ", type_init)
 
 while (i > iold):
-    print i
+    print(i)
     iold = i
     i *= 2
 
     if (not type(i) == type_init):
-        print "type changed, now: ", type(i)
+        print("type changed, now: ", type(i))
         break
 
-print i
+print(i)
