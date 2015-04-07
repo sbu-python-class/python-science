c527cd9d3a9a890748ba9e5f9591c9a4ec5b6409
Michael Zingale <michael.zingale@stonybrook.edu>
2015-04-06 21:29:54 -0400

    add limits

--- examples/GUI/plotter.py
+++ examples/GUI/plotter.py
@@ -19,30 +19,7 @@ if sys.version_info[0] < 3:
 else:
     import tkinter as Tk
 
-root = Tk.Tk()
-root.wm_title("plotter")
-
-
-frame1 = Tk.Frame(root)
-frame1.pack()
-
-Tk.Label(frame1, text="f(x) = ").grid(row=0, column=0, sticky=Tk.W)
-v = Tk.StringVar()
-func = Tk.Entry(frame1, textvariable=v)
-func.grid(row=0, column=1, sticky=Tk.W)
-
-
-# a tk.DrawingArea
-f = Figure(figsize=(5,4), dpi=100)
-a = f.add_subplot(111)
-canvas = FigureCanvasTkAgg(f, master=root)
-canvas.show()
-canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
-
-#toolbar = NavigationToolbar2TkAgg( canvas, root )
-#toolbar.update()
-#canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
-
+import tkFont
 
 def str_to_f(str):
     a = parse_expr(str, evaluate=0)
@@ -54,25 +31,77 @@ def _plot():
     f_str = func.get()
     f = str_to_f(f_str)
 
-    xv = np.linspace(0, 1, 1000)
+    xm = float(xmin.get())
+    xM = float(xmax.get())
+    
+    xv = np.linspace(xm, xM, 1000)
     fv = f(xv)
 
     a.plot(xv, fv)
+    a.set_xlim([xm, xM])
     canvas.show()
 
+    
 def _quit():
     print func.get()
     root.quit()     # stops mainloop
     root.destroy()  # this is necessary on Windows to prevent
                     # Fatal Python Error: PyEval_RestoreThread: NULL tstate
 
+    
+root = Tk.Tk()
+root.wm_title("plotter")
+
+
+myfont = tkFont.Font(size=14)
+
+frame1 = Tk.Frame(root)
+frame1.pack(fill=Tk.BOTH, expand=1)
+
+label = Tk.Label(frame1, text="f(x) = ", font=myfont)
+label.grid(row=0, column=0, sticky=Tk.W)
+v = Tk.StringVar()
+func = Tk.Entry(frame1, textvariable=v, font=myfont)
+func.grid(row=0, column=1, sticky=Tk.W)
+
+
+
+frame_extrema = Tk.Frame(root)
+frame_extrema.pack(fill=Tk.BOTH, expand=1)
+
+xmin_label = Tk.Label(frame_extrema, text="xmin: ", font=myfont)
+xmin_label.grid(row=0, column=0, sticky=Tk.W)
+xmin_v = Tk.StringVar()
+xmin = Tk.Entry(frame_extrema, textvariable=xmin_v, font=myfont)
+xmin_v.set("0.0")
+xmin.grid(row=0, column=1, sticky=Tk.W)
+
+xmax_label = Tk.Label(frame_extrema, text="xmax: ", font=myfont)
+xmax_label.grid(row=0, column=2, sticky=Tk.W)
+xmax_v = Tk.StringVar()
+xmax = Tk.Entry(frame_extrema, textvariable=xmax_v, font=myfont)
+xmax_v.set("1.0")
+xmax.grid(row=0, column=3, sticky=Tk.W)
+
+
+# a tk.DrawingArea
+f = Figure(figsize=(6,5), dpi=100)
+a = f.add_subplot(111)
+canvas = FigureCanvasTkAgg(f, master=root)
+canvas.show()
+canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+
+#toolbar = NavigationToolbar2TkAgg( canvas, root )
+#toolbar.update()
+#canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+
 frame2 = Tk.Frame(root)
 frame2.pack(fill=Tk.BOTH, expand=1)
 
-plot_button = Tk.Button(frame2, text='Plot', command=_plot)
+plot_button = Tk.Button(frame2, text='Plot', command=_plot, font=myfont)
 plot_button.pack(side=Tk.BOTTOM)
 
-quit_button = Tk.Button(frame2, text='Quit', command=_quit)
+quit_button = Tk.Button(frame2, text='Quit', command=_quit, font=myfont)
 quit_button.pack(side=Tk.BOTTOM)
 
 plot_button.pack(side=Tk.LEFT, padx=5, pady=5)
