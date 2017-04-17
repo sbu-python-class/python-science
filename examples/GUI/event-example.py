b725dfa75615b59571602437a11068a86f458d58
Michael Zingale <michael.zingale@stonybrook.edu>
2017-04-17 09:24:57 -0400

    update for python 3

--- examples/GUI/event-example.py
+++ examples/GUI/event-example.py
@@ -1,5 +1,7 @@
 # a simple example of an event loop that implements a CLI inventory
 # system
+#
+# requires python 3
 
 import sys
 
@@ -15,17 +17,17 @@ inventory = {}
 
 while True:
 
-    a = raw_input("\nCommand (or Enter 'h' for help)\n>> ")
+    a = input("\nCommand (or Enter 'h' for help)\n>> ")
 
     parts = a.split()
     cmd = parts.pop(0)
     
     if cmd in ["h", "help"]:
-        print help_str
+        print(help_str)
 
     elif cmd in ["a", "add"]:
         if not len(parts) == 2:
-            print "invalid add"
+            print("invalid add")
         else:
             if not parts[0] in inventory.keys():
                 inventory[parts[0]] = int(parts[1])
@@ -34,16 +36,16 @@ while True:
             
     elif cmd in ["r", "remove"]:
         if not len(parts) == 2:
-            print "invalid remove"
+            print("invalid remove")
         else:
             if not parts[0] in inventory.keys():
-                print "unable to remove -- no items in inventory"
+                print("unable to remove -- no items in inventory")
             else:
                 inventory[parts[0]] -= int(parts[1])
 
     elif cmd in ["l", "list"]:
-        for k, v in inventory.iteritems():
-            print "{:30} {:10}".format(k, v)
+        for k, v in inventory.items():
+            print("{:30} {:10}".format(k, v))
 
     elif cmd in ["q", "quit"]:
         sys.exit()
