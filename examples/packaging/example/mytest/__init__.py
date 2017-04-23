12de5d5c1ca661c72b8fd6bc7c376aaaa6d2c777
Michael Zingale <michael.zingale@stonybrook.edu>
2017-04-23 19:28:25 -0400

    comment for example

--- examples/packaging/example/mytest/__init__.py
+++ examples/packaging/example/mytest/__init__.py
@@ -2,6 +2,6 @@
 This is my simple test module for demonstrating packaging
 """
 
-from mytest.demo import demo
-from mytest.util import msg
+#from mytest.demo import demo
+#from mytest.util import msg
 
