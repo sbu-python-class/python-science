83309ba030ec3b7921b05224c464fc5d2e37477a
Michael Zingale <michael.zingale@stonybrook.edu>
2017-04-23 18:44:54 -0400

    single file example

---
+++ examples/packaging/single_file/example.py
@@ -0,0 +1,10 @@
+_hidden = "you may not see this"
+
+def demo():
+    print("this is a single file demo")
+
+
+if __name__ == "__main__":
+    print("in the main block -- you are running from the commandline")
+    demo()
+
