d345c372e2ea9f927841d44eef25163a98bad780
Michael Zingale <michael.zingale@stonybrook.edu>
2017-04-23 18:16:30 -0400

    a simple example

---
+++ examples/packaging/example/mytest/demo/demo.py
@@ -0,0 +1,5 @@
+def show():
+    print("this is my demo")
+
+def test(x):
+    print("x = {}".format(x))
---
+++ examples/packaging/example/mytest/util/msg.py
@@ -0,0 +1,5 @@
+def fail(s):
+    print("ERROR: {}".format(s))
+
+def warn(s):
+    print("WARNING: {}".format(s))
