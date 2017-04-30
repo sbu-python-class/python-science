39555c5b494f575fba32231d8045f2998e258a8c
Michael Zingale <michael.zingale@stonybrook.edu>
2017-04-30 19:32:00 -0400

    class example

--- examples/testing/pytest/class/test_class.py
+++ examples/testing/pytest/class/test_class.py
@@ -1,14 +1,15 @@
 # a test class is useful to hold data that we might want setup
 # for every test.
 
-
+import numpy as np
+from numpy.testing import assert_array_equal
 
 class TestClassExample(object):
 
     @classmethod
     def setup_class(cls):
         """ this is run once for each class, before any tests """
-        print("testing class {}".format(cls))
+        pass
 
     @classmethod
     def teardown_class(cls):
@@ -17,17 +18,14 @@ class TestClassExample(object):
 
     def setup_method(self):
         """ this is run before each of the test methods """
-        self.l = list(range(20))
-        self.s = "this is my test string"
+        self.a = np.arange(24).reshape(6, 4)
 
     def teardown_method(self):
         """ this is run after each of the test methods """
         pass
 
-    def test_list(self):
-        assert len(self.l) == 20
+    def test_max(self):
+        assert self.a.max() == 23
 
-    def test_string(self):
-        assert len(self.s.split()) == 5
-
-
+    def test_flat(self):
+        assert_array_equal(self.a.flat, np.arange(24))
