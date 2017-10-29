1e0abe9a0fdcce5284fa52114af57c1cdef80d21
Michael Zingale <michael.zingale@stonybrook.edu>
2017-10-29 16:04:55 -0400

    add python tutor

--- examples/matplotlib/fractal/mandelbrot.py
+++ examples/matplotlib/fractal/mandelbrot.py
@@ -23,14 +23,22 @@ xmax = 0.5
 ymin = -0.75
 ymax = -0.25
 
+# nice spot
 xc = -0.7435669
 yc = 0.1314023
 dx = 0.0022878
 
-xmin = xc-dx
-xmax = xc+dx
-ymin = yc-dx
-ymax = yc+dx
+xmin = xc-0.5*dx
+xmax = xc+0.5*dx
+ymin = yc-0.5*dx
+ymax = yc+0.5*dx
+
+
+# dagger
+#xmin = -0.8
+#xmax = -0.7
+#ymin = 0.0
+#ymax = 0.1
 
 x = np.linspace(xmin, xmax, N, dtype=np.float)
 y = np.linspace(ymin, ymax, N, dtype=np.float)
@@ -43,13 +51,13 @@ z = np.zeros((N,N), dtype=np.complex)
 niter = np.zeros((N,N), dtype=np.int)
 niter[:,:] = -1
 
-MAX_ITER = 256
+MAX_ITER = 1024
 
 for n in range(MAX_ITER):
     # potentially faster
-    #idx = niter == -1
-    #z[idx] = z[idx]**2 + c[idx]
-    z = z**2 + c
+    idx = niter == -1
+    z[idx] = z[idx]**2 + c[idx]
+    #z = z**2 + c
     idx = np.logical_and(abs(z) > 2, niter == -1)
     niter[idx] = n
 
@@ -58,7 +66,13 @@ niter[niter == -1] = MAX_ITER-1
 
 print(niter.min(), niter.max())
 
-plt.imshow(niter.T, extent=[xmin, xmax, ymin, ymax], origin="lower", cmap="magma_r")
+# we should do the colors based on putting equal numbers of points in
+# the plot for each color value, using a histogram
+hist, edges = np.histogram(niter, range=(0, MAX_ITER-1), bins=256)
+centers = 0.5*(edges[1:] + edges[:-1])
+
+
+plt.imshow(niter.T, extent=[xmin, xmax, ymin, ymax], origin="lower", cmap="gnuplot2") #cmap="magma_r")
 f = plt.gcf()
 f.set_size_inches(8.0, 8.0)
 #plt.show()
