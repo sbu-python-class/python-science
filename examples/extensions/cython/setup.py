3ae8ed78458537afb3fcf14a286a70d4afa83cf1
Michael Zingale <michael.zingale@stonybrook.edu>
2022-05-01 20:39:39 -0400

    more updates

---
+++ examples/extensions/cython/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+from Cython.Build import cythonize
+
+setup(name="mandel",
+      ext_modules=cythonize("mandel.pyx"),
+      zip_safe=False)
+
+
---
+++ examples/extensions/python/mandel.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def mandelbrot(N,
+               xmin=-2.0, xmax=2.0,
+               ymin=-2.0, ymax=2.0,
+               max_iter=10):
+
+    x = np.linspace(xmin, xmax, N)
+    y = np.linspace(ymin, ymax, N)
+
+    xv, yv = np.meshgrid(x, y, indexing="ij")
+
+    c = xv + 1j*y
+
+    z = np.zeros((N, N), dtype=np.complex128)
+
+    m = np.zeros((N, N), dtype=np.int)
+
+    for i in range(max_iter):
+        z = z**2 + c
+
+        m[np.logical_and(np.abs(z) > 2, m == 0)] = i
+
+    return m
