1f1ee52fea0753a0912aa8fafc534e7e70c5b612
Michael Zingale <michael.zingale@stonybrook.edu>
2024-04-24 10:11:20 -0400

    delambdaify

--- examples/extensions/pybind11/mandel.cpp
+++ examples/extensions/pybind11/mandel.cpp
@@ -13,74 +13,75 @@ using cmplx_arr = std::vector<std::vector<std::complex<double>>>;
 
 using namespace std::complex_literals;
 
-PYBIND11_MODULE(mandel, m) {
-    m.doc() = "C++ Mandelbrot example";
 
-    m.def("mandelbrot", [](int N,
-                           double xmin, double xmax,
-                           double ymin, double ymax, int max_iter) {
+py::array_t<int> mandelbrot(int N,
+                            double xmin, double xmax,
+                            double ymin, double ymax, int max_iter) {
 
-        // construct the numpy array we will return
-        // we need to specify the strides manually
+    // construct the numpy array we will return
+    // we need to specify the strides manually
 
-        constexpr size_t elsize = sizeof(int);
-        size_t shape[2]{N, N};
-        size_t strides[2]{N * elsize, elsize};
-        auto m = py::array_t<int>(shape, strides);
-        auto m_view = m.mutable_unchecked<2>();
+    constexpr size_t elsize = sizeof(int);
+    size_t shape[2]{N, N};
+    size_t strides[2]{N * elsize, elsize};
+    auto m = py::array_t<int>(shape, strides);
+    auto m_view = m.mutable_unchecked<2>();
 
+    // for the other arrays used only here, we can
+    // do whatever we want.  Since we can't yet rely
+    // on C++23 mdspan, we'll just do a vector of vectors
 
-        // for the other arrays used only here, we can
-        // do whatever we want.  Since we can't yet rely
-        // on C++23 mdspan, we'll just do a vector of vectors
+    std::vector<double> x(N, 0.0);
+    std::vector<double> y(N, 0.0);
 
-        std::vector<double> x(N, 0.0);
-        std::vector<double> y(N, 0.0);
+    double dx = (xmax - xmin) / static_cast<double>(N - 1);
+    double dy = (ymax - ymin) / static_cast<double>(N - 1);
 
-        double dx = (xmax - xmin) / static_cast<double>(N - 1);
-        double dy = (ymax - ymin) / static_cast<double>(N - 1);
+    for (int i = 0; i < N; ++i) {
+        x[i] = xmin + static_cast<double>(i) * dx;
+        y[i] = ymin + static_cast<double>(i) * dy;
+    }
 
-        for (int i = 0; i < N; ++i) {
-            x[i] = xmin + static_cast<double>(i) * dx;
-            y[i] = ymin + static_cast<double>(i) * dy;
+    cmplx_arr c(N, std::vector<std::complex<double>>(N, 0.0));
+    cmplx_arr z(N, std::vector<std::complex<double>>(N, 0.0));
+
+    // initialize c;
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            c[i][j] = x[i] + 1i * y[j];
         }
+    }
 
-        cmplx_arr c(N, std::vector<std::complex<double>>(N, 0.0));
-        cmplx_arr z(N, std::vector<std::complex<double>>(N, 0.0));
+    // zero out the output array
 
-        // initialize c;
-
-        for (int i = 0; i < N; ++i) {
-          for (int j = 0; j < N; ++j) {
-              c[i][j] = x[i] + 1i * y[j];
-          }
+    for (int i = 0; i < m.shape(0); ++i) {
+        for (int j = 0; j < m.shape(1); ++j) {
+            m_view(i, j) = 0;
         }
+    }
 
-        // zero out the output array
+    for (int niter = 1; niter <= max_iter; ++niter) {
 
         for (int i = 0; i < m.shape(0); ++i) {
-          for (int j = 0; j < m.shape(1); ++j) {
-              m_view(i, j) = 0;
-          }
-        }
+            for (int j = 0; j < m.shape(1); ++j) {
 
-        for (int niter = 1; niter <= max_iter; ++niter) {
+                if (m_view(i, j) == 0) {
+                    z[i][j] = z[i][j] * z[i][j] + c[i][j];
 
-            for (int i = 0; i < m.shape(0); ++i) {
-                for (int j = 0; j < m.shape(1); ++j) {
-
-                    if (m_view(i, j) == 0) {
-                        z[i][j] = z[i][j] * z[i][j] + c[i][j];
-
-                        if (std::abs(z[i][j]) > 2) {
-                            m_view(i, j) = niter;
-                        }
+                    if (std::abs(z[i][j]) > 2) {
+                        m_view(i, j) = niter;
                     }
                 }
             }
         }
+    }
 
-        return m;
-    });
+    return m;
 }
 
+
+PYBIND11_MODULE(mandel, m) {
+    m.doc() = "C++ Mandelbrot example";
+    m.def("mandelbrot", &mandelbrot);
+}
