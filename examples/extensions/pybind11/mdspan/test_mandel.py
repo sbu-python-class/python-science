b653b9e19a9c8c953d9f5e86f486a97e23378974
Michael Zingale <michael.zingale@stonybrook.edu>
2026-04-29 13:46:18 -0400

    using mdspan -- this is slower

---
+++ examples/extensions/pybind11/mdspan/test_mandel.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import mandel
+
+import time
+
+start = time.time()
+
+xmin = -2.5
+xmax = 1.5
+ymin = -2.0
+ymax = 2.0
+
+max_iter = 50
+
+m = mandel.mandelbrot(1024, xmin, xmax, ymin, ymax, max_iter)
+
+print(f"execution time = {time.time() - start}\n")
+
+
+fig, ax = plt.subplots()
+ax.imshow(np.transpose(m), origin="lower",
+          extent=[xmin, xmax, ymin, ymax])
+
+fig.tight_layout()
+fig.savefig("test.png")
